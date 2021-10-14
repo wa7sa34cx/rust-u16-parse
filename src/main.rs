2fbd61274e31f305d20561f474b0a2f31def05db
Alex Black <wa7@sa34.cx>
2021-10-14 16:12:55 +0300

    Add some examples

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
-    println!("Hello, world!");
+    let items: [&str; 5] = ["-10", "Hello, world!", "42", "65536", "65535"];
+
+    for item in items {
+        match item.parse::<u16>() {
+            Ok(num) => println!("{}", num),
+            Err(e) => println!("{}", e.to_string()),
+        }
+    }
 }
